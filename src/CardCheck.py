957db843c2772923c9466848c3c14743d44bec2c
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-05-16 14:21:07 +0100

    Update CardCheck.py

--- src/CardCheck.py
+++ src/CardCheck.py
@@ -1,15 +1,7 @@
-from datetime import date
+from datetime import datetime, date
 
-
-#Works
-def getnumber():
-    card_number=input("What is your card number?")
-        
-    return card_number
-
-#Works
-def luhn_checksum():
-    card_no=getnumber()
+def card_number():
+    card_no=input("Enter your payment card number: ")
     def digits_of(card_no):
         return [int(d) for d in str(card_no)]
     digits = digits_of(card_no)
@@ -24,26 +16,23 @@ def luhn_checksum():
     else:
         print("Card number is invalid")
     
-luhn_checksum()
+card_number()
 
 
-def getcarddate():
+def card_date():
     
-    date_entry = input('Enter the expiry date of the  in DD-MM-YYYY format: ')
-    year, month, day = map(int, date_entry.split('-'))
-    
-    # NameError: name 'datetime' is not defined
-    carddate = datetime.date(day, month, year)
-    return getcarddate
+    now = datetime.now()
+    now = date(year=now.year, month=now.month, day=1)
 
-def check_date():
-    
-    carddate=getcarddate()
-    """Checks current date against the credit card's date. If it is valid, returns True; else False."""
-    carddate = datetime.datetime.strptime(self.card_date, "%d/%m/%Y").date()
-    if datetime.date.today() < carddate:
-        print("This card has not expired")
-    else:
-        print("This card has expired")
+    card_expiry = input("Enter your payment card expiry date (MM/YY): ")
+    expiry = datetime.strptime(card_expiry, "%m/%y")
+    expiry = date(year=expiry.year, month=expiry.month, day=1)
 
-check_date()
+    if now == expiry:
+        print("Card is about to expire.")
+    elif now > expiry:
+        print("Card has expired.")
+    elif now < expiry:
+        print("Card has not expired.")
+    
+card_date()
